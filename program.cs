9631464297768419aa0bd1fc6b049c02179db855
Super Name <you@example.com>
2022-04-21 16:45:58 +0200

    f

--- program.cs
+++ program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using static UtilityLibraries.TemplateLibrary;
 class Program 
 {
     static void Main()
     {
         System.Console.WriteLine("ruft TemplateLibrary auf, um dann etwas zu tun:");
-        // TemplateLibrary.DoSomething();
+        DoSomething();
     }
 }
